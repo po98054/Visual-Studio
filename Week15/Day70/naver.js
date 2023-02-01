0a10419398768107a86b7e3bb88cf54f4ec57b67
po98054 <po98054@naver.com>
2023-02-01 21:33:27 +0900

    2023년 2월 1일 최종 수업 정리

--- Week15/Day70/naver.js
+++ Week15/Day70/naver.js
@@ -34,25 +34,43 @@ function moveRight(liSelector, ulSelector, time){
 
 	$(function(){
 
-		$('.container').hover(function(){
+		$('.box-body-right2').hover(function(){
 
 			//마우스가 요소 안으로 들어오면
 			//애니메이션이 진행중인 setInterval를 해제 => clearInterval
 
-			clearInterval(id);
+			clearInterval(rollingRight2);
 
 		}, function(){
 
 			//마우스가 요소를 빠져나가면
 			//setInterval를 통해 애니메이션이 진행되도록 작업
 
-			id = rolling();
+			rollingRight2 = rollingLeft('.box-body-right2 .item-stock', '.box-body-right2 .list-stock', 1000, 2000);
+
+		});
+
+		$('.box-body-left2 .list-issue').hover(function(){
+
+			//마우스가 요소 안으로 들어오면
+			//애니메이션이 진행중인 setInterval를 해제 => clearInterval
+
+			clearInterval(rollingLeft2);
+
+		}, function(){
+
+			//마우스가 요소를 빠져나가면
+			//setInterval를 통해 애니메이션이 진행되도록 작업
+
+			rollingLeft2 = rollingTop('.box-body-left2 .item-issue', '.box-body-left2 .list-issue', 1000, 2000);
 
 		});
 
 	})
 
 	let rollingRight2 = rollingLeft('.box-body-right2 .item-stock', '.box-body-right2 .list-stock', 1000, 2000);
+
+	let rollingLeft2 = rollingTop('.box-body-left2 .item-issue', '.box-body-left2 .list-issue', 1000, 2000)
 
 	function rolling(){
 
@@ -106,7 +124,21 @@ function rollingLeft(liSelector, ulSelector, animationTime, duration){
 
 function moveTop(liSelector, ulSelector, animationTime){
 
+	let height = $(liSelector).first().innerHeight();
 
+	if(!$(liSelector).first().is(':animated')){
+
+		$(liSelector).first().animate({
+
+			marginTop : `-${height}px`
+
+		}, animationTime, function(){
+
+			$(this).detach().appendTo(ulSelector).removeAttr('style');
+
+		})
+
+	}
 
 }
 
@@ -115,3 +147,43 @@ function rollingTop(liSelector, ulSelector, animamtionTime, duration){
 	return setInterval(moveTop, duration, liSelector, ulSelector, animamtionTime);
 
 }
+
+$(function(){
+
+	$('.box-shop-in .btn-prev').click((e) => {
+
+		e.preventDefault();
+
+		let boxShopIn = $(this).parents('.box-shop-in')
+
+		let curPage = boxShopIn.find('.current-page').first().text();
+
+		let maxPage = boxShopIn.find('.max-page').first().text();
+
+		curPage = +curPage;
+
+		curPage = --curPage == 0 ? maxPage : curPage;
+
+		boxShopIn.find('.current-page').text(curPage);
+
+	});
+
+	$('.box-shop-in .btn-next').click((e) => {
+
+		e.preventDefault();
+
+		let boxShopIn = $(this).parents('.box-shop-in')
+
+		let curPage = boxShopIn.find('.current-page').first().text();
+
+		let maxPage = boxShopIn.find('.max-page').first().text();
+
+		curPage = +curPage;
+
+		curPage = ++curPage > maxPage ? 1 : curPage;
+
+		boxShopIn.find('.current-page').text(curPage);
+
+	});
+
+})
